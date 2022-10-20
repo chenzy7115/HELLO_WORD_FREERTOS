4860528f066950df1cd5cca049645ed1cdcf8108
chenzy7115 <swczy@126.com>
2022-10-20 22:49:09 +0800

    suspend test

--- main/hello_world_main.c
+++ main/hello_world_main.c
@@ -13,23 +13,56 @@
 #include "esp_system.h"
 #include "esp_spi_flash.h"
 
-static const char *pcTxt = "I am CHENZY.";
-
-void myTask(void *pvParam)
+void myTask1(void *pvParam)
 {
-    char *pcTxtInTask;
-    pcTxtInTask = pvParam;
-
-    printf("I got message: %s\n", pcTxtInTask);
-        
-    vTaskDelay(1000 / portTICK_PERIOD_MS);
+    printf("Test Begin...\n");
+    vTaskSuspendAll();
+    for (int i = 0; i < 9999; i++)
+    {
+        for (int j = 0; j < 4999; j++)
+        {
+            ;
+        }
+    }
+    xTaskResumeAll();
+    printf("Test End...\n");
 
     vTaskDelete(NULL);
+    // while (1)
+    // {
+    //     printf("task:111\n");
+    //     vTaskDelay(1000 / portTICK_PERIOD_MS);
+
+    // }
+}
+
+void myTask2(void *pvParam)
+{
+    while (1)
+    {
+        printf("task:222\n");
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
 }
 
 void app_main(void)
 {
+    // UBaseType_t iPriority = 0;
+    TaskHandle_t pxTask1 = NULL;
 
-    xTaskCreate(myTask, "myTask1", 2048, (void *) pcTxt, 1, NULL);
+    xTaskCreate(myTask1, "myTask1", 2048, NULL, 1, &pxTask1); //创建一个task
+    xTaskCreate(myTask2, "myTask1", 2048, NULL, 2, NULL);     //创建一个task
+
+    // vTaskSuspend(pxTask1);
+    // vTaskDelay(5000 / portTICK_PERIOD_MS);
+    // vTaskResume(pxTask1);
+
+    // vTaskDelay(3000 / portTICK_PERIOD_MS);
+    // vTaskSuspend(pxTask1);
+
+    // vTaskPrioritySet(pxTask, 3);
+
+    // iPriority = uxTaskPriorityGet(pxTask); //通过句柄获得task的优先级
+
+    // printf("Task1's value : iPriority : %d\n", iPriority);
 }
-
