b0242d7e7d91813c003449cace8f5e5590811000
chenzy7115 <swczy@126.com>
2022-10-21 11:59:58 +0800

    add watch dog

--- main/hello_world_main.c
+++ main/hello_world_main.c
@@ -13,34 +13,24 @@
 #include "esp_system.h"
 #include "esp_spi_flash.h"
 
+#include "esp_task_wdt.h"//增加watch dog控制函数
+
 void myTask1(void *pvParam)
 {
-    printf("Test Begin...\n");
-    vTaskSuspendAll();
-    for (int i = 0; i < 9999; i++)
+    while (1)
     {
-        for (int j = 0; j < 4999; j++)
-        {
-            ;
-        }
+        printf("task:111\n");
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
     }
-    xTaskResumeAll();
-    printf("Test End...\n");
-
-    vTaskDelete(NULL);
-    // while (1)
-    // {
-    //     printf("task:111\n");
-    //     vTaskDelay(1000 / portTICK_PERIOD_MS);
-
-    // }
 }
 
 void myTask2(void *pvParam)
 {
+    esp_task_wdt_add(NULL);
     while (1)
     {
         printf("task:222\n");
+        esp_task_wdt_reset();
         vTaskDelay(1000 / portTICK_PERIOD_MS);
     }
 }
@@ -49,20 +39,30 @@ void app_main(void)
 {
     // UBaseType_t iPriority = 0;
     TaskHandle_t pxTask1 = NULL;
+    TaskHandle_t pxTask2 = NULL;
 
     xTaskCreate(myTask1, "myTask1", 2048, NULL, 1, &pxTask1); //创建一个task
-    xTaskCreate(myTask2, "myTask1", 2048, NULL, 2, NULL);     //创建一个task
+    xTaskCreate(myTask2, "myTask2", 2048, NULL, 1, &pxTask2); //创建一个task
 
-    // vTaskSuspend(pxTask1);
-    // vTaskDelay(5000 / portTICK_PERIOD_MS);
-    // vTaskResume(pxTask1);
+    UBaseType_t uxStack1;
 
-    // vTaskDelay(3000 / portTICK_PERIOD_MS);
-    // vTaskSuspend(pxTask1);
-
-    // vTaskPrioritySet(pxTask, 3);
-
-    // iPriority = uxTaskPriorityGet(pxTask); //通过句柄获得task的优先级
-
-    // printf("Task1's value : iPriority : %d\n", iPriority);
+    // while (1)
+    // {
+    //     uxStack1 = uxTaskGetStackHighWaterMark(pxTask1);
+    //     printf("uxStack1:%d\n", uxStack1);
+    //     vTaskDelay(3000 / portTICK_PERIOD_MS);
+    // }
 }
+
+// vTaskSuspend(pxTask1);
+// vTaskDelay(5000 / portTICK_PERIOD_MS);
+// vTaskResume(pxTask1);
+
+// vTaskDelay(3000 / portTICK_PERIOD_MS);
+// vTaskSuspend(pxTask1);
+
+// vTaskPrioritySet(pxTask, 3);
+
+// iPriority = uxTaskPriorityGet(pxTask); //通过句柄获得task的优先级
+
+// printf("Task1's value : iPriority : %d\n", iPriority);
